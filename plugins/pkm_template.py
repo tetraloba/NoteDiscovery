d01be4a6c6c423882d0ae69ed6baf0da6ff68616
tetraloba <tetraloba@gmail.com>
2026-09-13 19:57:35 +0900

    plugins pkm_template: Front Matter format (German2)

--- plugins/pkm_template.py
+++ plugins/pkm_template.py
@@ -6,41 +6,73 @@ NoteDiscovery has Template system, however it doesn't have advanced features
 """
 
 import logging
+from os import path
+import copy
 import re
+from datetime import datetime
 
 import yaml
 import markdown
 
 logger = logging.getLogger('__name__')
 
+# to format Front Matter
+class QuotedStr(str):
+    pass
+class Title(QuotedStr):
+    pass
+# customize PyYAML dumper
+class CustomDumper(yaml.Dumper):
+    # enforce indent (avoid list indentless style)
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
+# force double quotes ("") for QuotedStr (include inheritance)
+yaml.add_multi_representer(
+    QuotedStr,
+    lambda dumper, data: dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"'),
+    Dumper=CustomDumper
+)
+# force isoformat for datetime
+yaml.add_representer(
+    datetime,
+    lambda dumper, data: dumper.represent_scalar('tag:yaml.org,2002:timestamp', data.isoformat()),
+    Dumper=CustomDumper
+)
+
 class Plugin:
     def __init__(self):
         self.name = "PKM Template"
         self.version = "1.0.0"
         self.enabled = True
-
     def setup(self, ctx):
         """Swap the module logger for the per-plugin one the host provides."""
         global logger
         logger = ctx.logger
-
+    def _format_note():
+        pass
     def on_note_create(self, note_path: str, initial_content: str) -> str | None:
         md = markdown.Markdown(extensions=['meta'])
-        html = md.convert(initial_content)
-
-        # flatten Meta values
-        for key in md.Meta.keys():
-            if isinstance(md.Meta[key], list) and len(md.Meta[key]) == 1:
-                md.Meta[key] = md.Meta[key][0]
-
-        logging.debug(md.Meta)
-        
-        TITLE_FROM_FILENAME = r'^"\d{8}-\d{6}_(.*)"$'
-        title_match = re.match(TITLE_FROM_FILENAME, md.Meta['title'])
+        _ = md.convert(initial_content)
+        # header
+        # header / title
+        TITLE_FROM_FILENAME = r'^\d{8}-\d{6}_(.*)\.md$'
+        filename = path.basename(note_path)
+        title_match = re.match(TITLE_FROM_FILENAME, filename)
         if title_match == None:
-            logging.warning(f"the filename format ({md.Meta['title']}) is not supported.")
+            logging.warning(f"the filename format ({filename}) is not supported.")
             return None
-        md.Meta['title'] = title_match.groups()[0]
-        new_yaml_text = yaml.safe_dump(md.Meta, allow_unicode=True, default_flow_style=False, sort_keys=False)
-        return f"---\n{new_yaml_text}---\n" + '\n'.join(md.lines)
-
+        md.Meta['title'] = Title(title_match.groups()[0])
+        # header / created and updated
+        dt_now = datetime.now()
+        md.Meta['created'] = dt_now
+        md.Meta['updated'] = copy.copy(dt_now) # to avoid YAML Anchor
+        header = yaml.dump(md.Meta, Dumper=CustomDumper, allow_unicode=True, sort_keys=False)
+        # body
+        if 'lines' in md.__dict__.keys():
+            logger.debug('md has lines.')
+            body = '\n'.join(md.lines)
+        else:
+            logger.debug('md doesn\'t have lines.')
+            body = '\n'
+        # dump
+        return f"---\n{header}---\n" + body
